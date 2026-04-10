2825915470331dbe11aedbf7f14ce68ede7b07e4
UP240373 <up240373@alumnos.upa.edu.mx>
2026-04-09 18:27:38 -0600

    feat: Create methods for users  - GET /users/filter = Filter users  - GET /users/{id} = Get one user with ID  - POST /users = Create new user  - PATCH /users/{id}/status = Update status

--- backend/setpoints.js
+++ backend/setpoints.js
@@ -22,7 +22,10 @@ app.use(cors({
   SETPOINTS
 
   GET /users = Obtener todos los usuarios
-
+  GET /users/filter = Filtrar usuarios
+  GET /users/{id} = Obtener usuario por ID
+  POST /users = Crear usuario
+  PATCH /users/{id}/status = Actualizar estado
 
 */
 
@@ -32,13 +35,111 @@ app.get('/users', (req, res) => {
 
   db.query(query, (err, users) => {
     if (err) {
-      return res.status(500).json({ error: "Not found connection with the base data", err});
+      return res.status(500).json({ error: "Database error", err});
     }
 
-    res.status(200).json({ message: "Get information successfully.", users});
-  })
+    res.status(200).json({ message: "Get information successfully", users});
+  });
 });
 
+// GET /users/filter = Filtrar usuarios
+app.get('/users/filter', (req, res) => {
+  const { name, email, career_id, rol } = req.query;
+  
+  let query = 'SELECT * FROM users WHERE 1 = 1';
+
+  if (name) {
+    query += ` AND name = "${name}"`;
+  }
+
+  if (email) {
+    query += ` AND email = "${email}"`;
+  }
+
+  if (career_id) {
+    query += ` AND career_id = "${career_id}"`;
+  }
+
+  if (rol) {
+    query += ` AND rol = "${rol}"`;
+  }
+  
+  db.query(query, (err, users) => {
+    if (err) {
+      return res.status(500).json({ error: "Database error", err });
+    }
+
+    if (users.length === 0) {
+      return res.status(404).json({ error: "User don't found"});
+    }
+
+    res.status(200).json({ message: "Success", users });
+  });
+});
+
+// POST /users = Crear usuario
+app.get('/users/:id', (req, res) => {
+  const query = `SELECT * FROM users WHERE id = ${req.params.id}`;
+
+  db.query(query, (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: "Database error", err });
+    }
+
+    if (user.length === 0) {
+      return res.status(404).json({ error: "The user doesn't exists" });
+    }
+
+    res.status(200).json({ message: "Get user successfully", user});
+  });
+});
+
+// POST /users = Crear usuario
+app.post('/users', (req, res) => {
+  const { name, last_name, username, email, career_id, active, password, rol, failed_attempts } = req.body;
+
+  const filter = `SELECT * FROM users WHERE email = "${email}" OR username = "${username}"`;
+  db.query(filter, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: "Database error", err });
+    }
+
+    if (result.length > 0) {
+      return res.status(409).json({ err: "The username or email already exists"});
+    }
+
+    const query = `INSERT INTO users (name, last_name, username, email, career_id, active, password, rol, failed_attempts) VALUES ("${name}", "${last_name}", "${username}", "${email}", ${career_id}, ${active}, "${password}", "${rol}", ${failed_attempts});`;
+    db.query(query, (err, user) => {
+      if (err) {
+        return res.status(500).json({ error: "Database error", err });
+      }
+      res.status(201).json({ message: "The user has been created successfully" });
+    })
+  });
+});
+
+// PATCH /users/{id}/status = Actualizar estado
+app.patch('/users/:id/status', (req, res) => {
+  const search = `SELECT * FROM users WHERE id = ${req.params.id}`;
+  db.query(search, (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: "Database error", err });
+    }
+
+    if (user.length === 0) {
+      return res.status(404).json({ error: "The user doesn't exists" });
+    }
+
+    const newStatus = user[0].active === 1 ? 0 : 1;
+    const query = `UPDATE users SET active = ${newStatus} WHERE id = ${req.params.id};`
+    db.query(query, (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: "Database error", err });
+      }
+      res.status(200).json({message: "The user's status has been updated" });
+    });
+  });
+});
 
 // Iniciar servidor
 app.listen(PORT, () => {
